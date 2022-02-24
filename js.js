9cdacb2b992b0a98fe2739dd7dfbbed879dc1df1
DeltaJoy <j.komorowski1991@gmail.com>
2022-02-24 16:15:54 -0600

    style 1

---
+++ js.js
@@ -0,0 +1,16 @@
+
+
+let target1 = document.getElementById("currentPage")
+let ass = document.getElementById("1")
+
+
+ass.addEventListener("click", switchA);
+
+function switchA () {
+    target1.ELEMENT_NODE.remove("currentPage")
+    ass.ELEMENT_NODE.add("currentPage")
+}
+
+
+
+console.log(target1)

a2cc46240e842cf80d2d61b675beafbcb8a7c99c
DeltaJoy <j.komorowski1991@gmail.com>
2022-03-13 11:17:29 -0500

    zoom when putting data on form

--- js.js
+++ js.js
@@ -1,16 +1,27 @@
+const emailCollector = document.getElementById("emailCollection")
 
+console.log(emailCollector)
 
-let target1 = document.getElementById("currentPage")
-let ass = document.getElementById("1")
+emailCollector.addEventListener("submit", event => {
+    event.preventDefault()
+    
+    let ourFormData = new FormData(event.target)
+    let userFirstName = ourFormData.get("firstName")
+    let userLastName = ourFormData.get("lastName")
+    let userEmailAddress = ourFormData.get("emailAddress")
+    let userSocialMediaHandles = ourFormData.get("socialMediaHandles")
+    let userHear = ourFormData.get("howDidYouHear")
+    let userGoals = ourFormData.get("yourFitnessGoals")
+    let userCommitted = ourFormData.get("areYouCommitted")
+    let userConcerns = ourFormData.get("additionalConcerns")
+    
+    console.log(userFirstName)
+    console.log(userLastName)
+    console.log(userEmailAddress)
+    console.log(userSocialMediaHandles)
+    console.log(userHear)
+    console.log(userGoals)
+    console.log(userCommitted)
+    console.log(userConcerns)
+})
 
-
-ass.addEventListener("click", switchA);
-
-function switchA () {
-    target1.ELEMENT_NODE.remove("currentPage")
-    ass.ELEMENT_NODE.add("currentPage")
-}
-
-
-
-console.log(target1)
